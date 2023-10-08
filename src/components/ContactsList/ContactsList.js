9622a40e400efc7e4827a1fc20bf0078ead77e04
Nataly-Naf <nat.spivak84@gmail.com>
2023-10-08 14:20:07 +0100

    all tasks

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,11 +1,26 @@
+import { List, ListItem, ListBtn } from './Contacts.styled';
 
-export const ContactsList = ({ contacts, pushName }) => {
-      if (contacts.length !== 0) {
-        console.log(contacts)
-        return <ul>
-            {contacts.map(contact => { return <li key={contact.id}> {contact.name}: {contact.number} </li> })
-            }
-        </ul>
-    }
-}
-
+export const ContactsList = ({ contacts, onDelete }) => {
+  if (contacts.length !== 0) {
+    console.log(contacts);
+    return (
+      <List>
+        {contacts.map(contact => {
+          return (
+            <ListItem key={contact.id}>
+              {' '}
+              {contact.name}: {contact.number}{' '}
+              <ListBtn
+                onClick={() => {
+                  onDelete(contact.id);
+                }}
+              >
+                Delete
+              </ListBtn>
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
 import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   
        <App />
-   
-   
+    
+      <GlobalStyle/>
+       
   </React.StrictMode>
 );
